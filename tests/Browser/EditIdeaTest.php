41e59b765b1148a7acf3c6c8c464282e3f292687
Vinicios Wentz <wentz.vinicios@gmail.com>
2026-07-04 20:17:48 -0300

    idea edit test

---
+++ tests/Browser/EditIdeaTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Models\Idea;
+use App\Models\User;
+
+it('can create a idea', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $idea = Idea::factory()->for($user)->create();
+
+    visit('/ideas')
+        ->click('@create-idea-button')
+        ->fill('title', 'Some idea example')
+        ->click('@button-status-completed')
+        ->fill('description', 'Some idea example')
+        ->fill('@new-link', 'https://example.com/ideas')
+        ->click('@submit-new-link-button')
+        ->fill('@new-link', 'https://example2.com/ideas')
+        ->click('@submit-new-link-button')
+        ->fill('@new-step', 'Step 1')
+        ->click('@submit-new-step-button')
+        ->fill('@new-step', 'Step 2')
+        ->click('@submit-new-step-button')
+        ->click('Create')
+        ->assertPathIs('/ideas');
+
+    /** @var Idea $idea */
+    $idea = $user->ideas()->first();
+
+    expect($idea)
+        ->toMatchArray([
+            'title' => 'Some idea example',
+            'status' => 'completed',
+            'description' => 'Some idea example',
+            'links' => [
+                'https://example.com/ideas',
+                'https://example2.com/ideas',
+            ],
+        ])
+        ->and($idea->steps)->toHaveCount(2);
+});
